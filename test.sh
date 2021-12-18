3b5893d93c1512c3972fc85f2ee4c72611f2bf2b
Denis Godoy <denisgodoy1@gmail.com>
2021-12-18 14:49:54 -0300

    update tests

--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 $(count=0)
-$(exit=0)
-$(exit_orig=0)
 $(rm -rf output)
 $(mkdir output)
 $(ls -la > infile)
@@ -11,11 +9,11 @@ echo '================================================================'
 echo '             Test' $count ">> Check the executable"
 echo '================================================================'
 if [ ! -f "pipex" ]; then
-	echo "$(tput setaf 1)name [KO]$(tput sgr 0)"
+	echo "$(tput setaf 1)executable [KO]$(tput sgr 0)"
 	echo
 	exit 0
 else
-	echo "$(tput setaf 2)name [OK]$(tput sgr 0)"
+	echo "$(tput setaf 2)executable [OK]$(tput sgr 0)"
 fi
 sleep 1
 
@@ -45,9 +43,14 @@ count=$((count+1))
 echo '================================================================'
 echo '        Test' $count ">> Invalid number of arguments"
 echo '================================================================'
-./pipex infile "cat" "grep $" "wc -l" "output/outfile${count}"
-valgrind --leak-check=full --log-file="output/valgrind${count}" ./pipex infile "cat" "grep $" "wc -l" output/valgrind >/dev/null 2>&1
+./pipex infile "cat -e" "grep $" "wc -l" "output/outfile${count}"
 echo
+if [ ! -f "output/valgrind${count}" ]; then
+    echo "$(tput setaf 2)check [OK]$(tput sgr 0)"
+else
+	echo "$(tput setaf 1)check [KO]$(tput sgr 0)"
+fi
+valgrind --leak-check=full --log-file="output/valgrind${count}" ./pipex infile "cat -e" "grep $" "wc -l" output/valgrind >/dev/null 2>&1
 if [ ! -f "output/valgrind${count}" ]; then
     echo "Valgrind not found!"
 else
@@ -66,11 +69,10 @@ count=$((count+1))
 echo '================================================================'
 echo '                Test' $count ">> Invalid infile"
 echo '================================================================'
-chmod 000 infile
-./pipex file1 "cat" "tr [a-z] [A-Z]" "output/outfile${count}"
+./pipex file1 "cat" "grep x" "output/outfile${count}"
 echo "exit code" $?
 exit=$?
-< file1 cat | tr [a-z] [A-Z] > "output/outfile${count}-orig"
+< file1 cat | grep x > "output/outfile${count}-orig"
 echo "exit code" $?
 exit_orig=$?
 echo
@@ -86,8 +88,7 @@ if [[ $ret -eq 0 ]]; then
 else
     echo "$(tput setaf 1)diff [KO]$(tput sgr 0)"
 fi
-valgrind --leak-check=full --log-file="output/valgrind${count}" ./pipex infile "cat" "tr [a-z] [A-Z]" output/valgrind >/dev/null 2>&1
-chmod 777 infile
+valgrind --leak-check=full --log-file="output/valgrind${count}" ./pipex file1 "cat" "grep x" output/valgrind >/dev/null 2>&1
 if [ ! -f "output/valgrind${count}" ]; then
     echo "Valgrind not found!"
 else
@@ -107,10 +108,10 @@ echo '================================================================'
 echo '              Test' $count ">> Infile bad permission"
 echo '================================================================'
 chmod 000 infile
-./pipex infile "cat" "tr [a-z] [A-Z]" "output/outfile${count}"
+./pipex infile "cat" "cat -e" "output/outfile${count}"
 echo "exit code" $?
 exit=$?
-< infile cat | tr [a-z] [A-Z] > "output/outfile${count}-orig"
+< infile cat | cat -e > "output/outfile${count}-orig"
 echo "exit code" $?
 exit_orig=$?
 echo
@@ -126,7 +127,7 @@ if [[ $ret -eq 0 ]]; then
 else
     echo "$(tput setaf 1)diff [KO]$(tput sgr 0)"
 fi
-valgrind --leak-check=full --log-file="output/valgrind${count}" ./pipex infile "cat" "tr [a-z] [A-Z]" output/valgrind >/dev/null 2>&1
+valgrind --leak-check=full --log-file="output/valgrind${count}" ./pipex infile "cat" "cat -e" output/valgrind >/dev/null 2>&1
 chmod 777 infile
 if [ ! -f "output/valgrind${count}" ]; then
     echo "Valgrind not found!"
@@ -148,12 +149,12 @@ echo '              Test' $count ">> Outfile bad permission"
 echo '================================================================'
 touch "output/outfile${count}"
 chmod 000 "output/outfile${count}"
-./pipex infile "cat" "tr [a-z] [A-Z]" "output/outfile${count}"
+./pipex infile "grep pipex" "wc -lw" "output/outfile${count}"
 echo "exit code" $?
 exit=$?
 touch "output/outfile${count}-orig"
 chmod 000 "output/outfile${count}-orig"
-< infile cat | tr [a-z] [A-Z] > "output/outfile${count}-orig"
+< infile grep pipex | wc -lw > "output/outfile${count}-orig"
 echo "exit code" $?
 exit_orig=$?
 chmod 777 "output/outfile${count}"
@@ -171,10 +172,10 @@ if [[ $ret -eq 0 ]]; then
 else
     echo "$(tput setaf 1)diff [KO]$(tput sgr 0)"
 fi
-if [ -f "output/outfile" ]; then
-	chmod 000 "output/outfile"
+if [ -f "output/valgrind" ]; then
+	chmod 000 "output/valgrind"
 fi
-valgrind --leak-check=full --log-file="output/valgrind${count}" ./pipex infile "cat" "tr [a-z] [A-Z]" output/valgrind >/dev/null 2>&1
+valgrind --leak-check=full --log-file="output/valgrind${count}" ./pipex infile "grep pipex" "wc -lw" output/valgrind >/dev/null 2>&1
 if [ ! -f "output/valgrind${count}" ]; then
     echo "Valgrind not found!"
 else
@@ -186,8 +187,8 @@ else
     	echo "$(tput setaf 1)memcheck [KO]$(tput sgr 0)"
 	fi
 fi
-if [ -f "output/outfile" ]; then
-	chmod 777 "output/outfile"
+if [ -f "output/valgrind" ]; then
+	chmod 777 "output/valgrind"
 fi
 sleep 1
 
@@ -196,10 +197,10 @@ count=$((count+1))
 echo '================================================================'
 echo '                Test' $count ">> Invalid command 1"
 echo '================================================================'
-./pipex infile "catzado" "tr [a-z] [A-Z]" "output/outfile${count}"
+./pipex infile "catzado" "wc -w" "output/outfile${count}"
 echo "exit code" $?
 exit=$?
-< infile catzado | tr [a-z] [A-Z] > "output/outfile${count}-orig"
+< infile catzado | wc -w > "output/outfile${count}-orig"
 echo "exit code" $?
 exit_orig=$?
 echo
@@ -215,7 +216,7 @@ if [[ $ret -eq 0 ]]; then
 else
     echo "$(tput setaf 1)diff [KO]$(tput sgr 0)"
 fi
-valgrind --leak-check=full --log-file="output/valgrind${count}" ./pipex infile "catzado" "tr [a-z] [A-Z]" output/valgrind >/dev/null 2>&1
+valgrind --leak-check=full --log-file="output/valgrind${count}" ./pipex infile "catzado" "wc -w" output/valgrind >/dev/null 2>&1
 if [ ! -f "output/valgrind${count}" ]; then
     echo "Valgrind not found!"
 else
@@ -234,10 +235,10 @@ count=$((count+1))
 echo '================================================================'
 echo '                Test' $count ">> Invalid command 2"
 echo '================================================================'
-./pipex infile "cat" "trzero [a-z] [A-Z]" "output/outfile${count}"
+./pipex infile "cat" "trzero a b" "output/outfile${count}"
 echo "exit code" $?
 exit=$?
-< infile cat | trzero [a-z] [A-Z] > "output/outfile${count}-orig"
+< infile cat | trzero a b > "output/outfile${count}-orig"
 echo "exit code" $?
 exit_orig=$?
 echo
@@ -253,7 +254,7 @@ if [[ $ret -eq 0 ]]; then
 else
     echo "$(tput setaf 1)diff [KO]$(tput sgr 0)"
 fi
-valgrind --leak-check=full --log-file="output/valgrind${count}" ./pipex infile "cat" "trzero [a-z] [A-Z]" output/valgrind >/dev/null 2>&1
+valgrind --leak-check=full --log-file="output/valgrind${count}" ./pipex infile "cat" "trzero a b" output/valgrind >/dev/null 2>&1
 if [ ! -f "output/valgrind${count}" ]; then
     echo "Valgrind not found!"
 else
@@ -329,7 +330,7 @@ if [[ $ret -eq 0 ]]; then
 else
     echo "$(tput setaf 1)diff [KO]$(tput sgr 0)"
 fi
-valgrind --leak-check=full --log-file="output/valgrind${count}" ./pipex infile "grep .c" "wc -l" output/valgrind >/dev/null 2>&1
+valgrind --leak-check=full --log-file="output/valgrind${count}" ./pipex infile "grep d" "cat -e" output/valgrind >/dev/null 2>&1
 if [ ! -f "output/valgrind${count}" ]; then
     echo "Valgrind not found!"
 else
